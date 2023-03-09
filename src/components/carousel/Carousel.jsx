f36a003c6eecd7b5d7f706aedbb0932d45de8ab5
pravinargade18 <pravinargade18@gmail.com>
2023-03-09 23:40:10 +0530

    Added carousel component

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,90 @@
+import React, { useRef } from "react";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+
+import "./Carousel.scss";
+
+const Carousel = ({data,loading}) => {
+
+    const carouselContainer = useRef();
+    const {url}=useSelector((state) =>state.home)
+
+    const navigate=useNavigate();
+
+    const navigation=(dir)=>{
+
+    }
+
+    const skItem=()=>{
+        return (
+            <div className="skeletonItem">
+                <div className="posterBlock skeleton">
+                    <div className="textBlock">
+                        <div className="title skeleton"></div>
+                        <div className="date skeleton"></div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+  return (
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRighttNav arrow"
+          onClick={() => navigation("right")}
+        />
+        {!loading ? (
+          <div className="carouselItems">
+            {data?.map((item) => {
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : PosterFallback;
+              return (
+                <div key={item.id} className="carouselItem">
+                  <div className="posterBlock">
+                    <Img src={posterUrl} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">
+                        {item.title || item.name}
+                    </span>
+                    <span className="title">
+                        {dayjs(item.release_Date).format("MMM D, YYYY")}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+                {skItem()}
+          </div>
+        )}
+      </ContentWrapper>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroBanner from './heroBanner/HeroBanner';
 import './Home.scss'
+import Trending from './trending/Trending';
 
 const Home = () => {
   return (
     <div className="homePage">
         <HeroBanner/>
+        <Trending/>
     </div>
   )
 }
