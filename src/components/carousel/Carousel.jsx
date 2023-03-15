3557dea138b5b2ac5dbe7c66f0b7457ed9bfbd86
pravinargade18 <pravinargade18@gmail.com>
2023-03-15 17:56:25 +0530

    Added scrolling function to carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -23,7 +23,14 @@ const Carousel = ({data,loading}) => {
     const navigate=useNavigate();
 
     const navigation=(dir)=>{
+        const container=carouselContainer.current;
 
+        const scrollAmount=dir==='left'?container.scrollLeft-(container.offsetWidth +20): container.scrollLeft + (container.offsetWidth + 20);
+
+        container.scrollTo({
+          left:scrollAmount,
+          behavior: 'smooth'
+        })
     }
 
     const skItem=()=>{
@@ -52,13 +59,13 @@ const Carousel = ({data,loading}) => {
           onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
                 : PosterFallback;
               return (
-                <div key={item.id} className="carouselItem">
+                <div key={item.id} className="carouselItem"  onClick={()=>navigate(`/${item.media_type}/${item.id}`)} >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
