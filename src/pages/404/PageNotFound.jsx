393431cb4e0ef8354694a4570d0b6843a0e37b36
pravinargade18 <pravinargade18@gmail.com>
2023-03-16 17:44:55 +0530

    Added ErrorPage

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './PageNotFound.scss'
+import React from "react";
+
+import "./PageNotFound.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
+};
 
 export default PageNotFound;
